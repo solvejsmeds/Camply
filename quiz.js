88f028c5a68521f6b6128b99d260eef2a5900b16
Solvej Smeds <ss226vt@student.lnu.se>
2025-05-13 11:02:01 +0200

    quiznavigering

--- quiz.js
+++ quiz.js
@@ -1,14 +1,47 @@
-function init(){
-    console.log ("hej") 
+function init() {
+    console.log("hej");
     goToPage();
 }
+window.addEventListener("load", init);
+
+function goToPage() {
+    const cityElement = document.querySelector("#goToStad");
+    const campingElement = document.querySelector("#goToCampingQuiz");
+    const attractionElement = document.querySelector("#goToAttraction");
+    const activityElement = document.querySelector("#goToActivity");
+    const barbequeElement = document.querySelector("#goToBarbeque");
+
+    if (cityElement) {
+        cityElement.addEventListener("click", function () {
+            window.location.href = "specifikquizstad.html";
+        });
+    }
+
+    if (campingElement) {
+        campingElement.addEventListener("click", function () {
+            window.location.href = "vilkenpassardig.html";
+        });
+    } 
+
+    if (attractionElement) {
+        attractionElement.addEventListener("click", function () {
+            window.location.href = "sevärdighet.html";
+        });
+    }
+
+    if (activityElement) {
+        activityElement.addEventListener("click", function () {
+            window.location.href = "aktiviteter.html";
+        });
+    }
+
+    if (barbequeElement) {
+        barbequeElement.addEventListener("click", function () {
+            window.location.href = "grilla.html";
+        });
+    }
 
 
-function goToPage(){
-    const element = document.getElementById("gotoStad");
-
-element.addEventListener("click", function() {
-window.location.href = "specifikquizstad.html";
-    });
-    element.appendChild(element)
+  
+ 
 }
