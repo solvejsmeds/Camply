835e786223b8f58a22f4123d54227e532c6e6010
Solvej Smeds <ss226vt@student.lnu.se>
2025-05-13 09:40:36 +0200

    filterkartaväder

--- filter.js
+++ filter.js
@@ -3,24 +3,41 @@ const APIkey = "R7PGDNjZ" //api nyckel till SMAPI
 
 let allCampings = []; //sparar alla cmapingar globalt
 
-//let locationMapping = {}; //lagrar json innehållet
+const positionIds = { //id för campingarna uppdelade i kategorier för olika lägen
+  forest: [ //skog
+    659, 658, 657, 656, 655, 616, 615, 614, 613, 612, 611, 610, 609, 607, 606, 605, 604, 603, 602, 601, 600, 307, 305, 608, 303, 201, 202, 301, 286, 285, 281, 280, 277, 275, 273, 246, 245, 244, 243, 242, 241, 216, 217, 218, 219, 220, 238, 237
+  ],
+  lake: [ //sjö
+    659, 658, 657, 656, 655, 616, 615, 614, 613, 612, 611, 610, 609, 607, 606, 605, 604, 603, 602, 601, 600, 307, 305, 608, 303, 301, 286, 285, 281, 280, 275, 273, 246, 245, 244, 239
+  ],
+  sea: [ //hav
+    170, 173, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 197, 198, 200, 201, 202, 205, 207, 209, 210, 211, 215, 277, 216, 243, 242, 241, 217, 218, 219, 220, 240, 238, 237, 236
+  ],
+  city: [ //stad
+    616, 184, 189, 191, 192, 303, 301, 247, 239, 238
+  ]
+};
+
+
 
 function init() {
 
+  fetchSmapiCities() //funktion anrop: hämtar städer till stad filtrer
 
-fetchSmapiCities() //funktion anrop: hämtar städer till stad filtrer
+  showCampings(); //anropar fuunktion som visar alla campingar
 
+  //document.querySelector("#selectedCity").addEventListener("change", filterCampings); //change lyssnare på stad filtrering
+  document.querySelector("#selectedPrice").addEventListener("change", filterCampings); //change lyssnare på pris filtrering
 
-showCampings(); //anropar fuunktion som visar alla campingar
+  dropdown(); //dropdown för läge filtrering
+  dropdownCity(); //dropdown för stad filtrering
 
-document.querySelector("#selectedCity").addEventListener("change", filterCampings); //change lyssnare på stad filtrering
-document.querySelector("#selectedPrice").addEventListener("change", filterCampings); //change lyssnare på pris filtrering
+  let positionCheckboxes = document.querySelectorAll("#dropdownContent input[type='checkbox']"); //hämtar alla checkboxar
 
-dropdown();
-
-//requestData("position.json")
-
-
+  for (let i = 0; i < positionCheckboxes.length; i++) {
+    let checkbox = positionCheckboxes[i];
+    checkbox.addEventListener("change", filterCampings);
+  } //loopar igenom checkboxarna och lägger till change eventlyssnare, anropar filter campings
 
 }
 window.addEventListener("load", init)
@@ -28,133 +45,199 @@ window.addEventListener("load", init)
 //-----------------------------------------------------------------------------------
 
 
-function dropdown() {//funktion för att hantera dropdown menyn med kryssruto för läge
-  let dropdownBtn = document.getElementById("dropdownBtn");
-  let dropdownContent = document.getElementById("dropdownContent");
 
-  dropdownBtn.addEventListener("click", function(event) {
-    event.stopPropagation();
-    dropdownContent.classList.toggle("show");
+//funktion för att hantera dropdown menyn med kryssruto för läge
+function dropdown() {
+  let dropdownBtn = document.querySelector("#dropdownBtn"); //knappen för läge
+
+  let dropdownContent = document.querySelector("#dropdownContent"); //hämtas dropdownmenyn med lägen
+
+  dropdownBtn.addEventListener("click", function (event) { //händelselyssnare på knappen för läge
+    event.stopPropagation(); //förhindrar bubbla upp
+    dropdownContent.classList.toggle("show"); //visar menun
   });
 
-  
 }
 //Slut dropodown
 //-----------------------------------------------------------------------------------
 
+
+
+function dropdownCity() {
+  let dropdownBtnCity = document.querySelector("#dropdownBtnCity");
+
+  let dropdownContentCity = document.querySelector("#dropdownContentCity");
+
+  dropdownBtnCity.addEventListener("click", function (event) {
+    event.stopPropagation();
+    dropdownContentCity.classList.toggle("show");
+  }); //lägger klicklyssnare på knappen så att dropdownen visas
+
+  // Stäng dropdown när man klickar utanför
+  document.addEventListener("click", function () {
+    dropdownContentCity.classList.remove("show");
+  });
+
+  dropdownContentCity.addEventListener("click", function (event) {
+    event.stopPropagation(); // förhindrar att klick inuti dropdown stänger den
+  });
+}
+//Slut dropodownCity
+//-----------------------------------------------------------------------------------
+
+
+
 //funktion för att visa alla campingar, hämtar alla cmapingar fårn smapi och sparar dem i "allCampings"
 async function showCampings() {
-    const campingDiv = document.querySelector("#campingsContainer"); //element för cmapingarna 
 
-    let response = await fetch ("https://smapi.lnu.se/api/?debug=true&api_key=R7PGDNjZ&controller=establishment&method=getall&descriptions=camping"); //anrop med fetch till Smapi, hämtar data
+  const campingDiv = document.querySelector("#campingsContainer"); //element för cmapingarna 
 
-const data = await response.json(); //omvandlar svaret till json objekt
+  let response = await fetch("https://smapi.lnu.se/api/?debug=true&api_key=R7PGDNjZ&controller=establishment&method=getall&descriptions=camping"); //anrop med fetch till Smapi, hämtar data
 
+  const data = await response.json(); //omvandlar svaret till json objekt
 
+  campingDiv.innerHTML = ""; //rensar
 
-campingDiv.innerHTML = "";
- 
-allCampings = data.payload; //sparar alla campingar i allCampings
-displayCampings(allCampings); //antopar funktion som visar cmapingarna
+  allCampings = data.payload; //sparar alla campingar i allCampings
+
+  displayCampings(allCampings); //antopar funktion som visar cmapingarna
 
 }
 //Slut showCampings
 //-----------------------------------------------------------------------------------
 
 
+
 //visar cmapingarna på sidan. 
 function displayCampings(campings) {
 
   const campingDiv = document.querySelector("#campingsContainer"); //hämtar elementet där cmapingarna ligger
+
   campingDiv.innerHTML = ""; //rensar html
 
- for (let i = 0; i < campings.length; i++) { 
-    const camping = campings[i];
-
- 
-
-    let container = document.createElement("div");
-    container.classList.add("campcontainer");
-
-    // Skapa HTML för varje camping
-    container.innerHTML += 
-     "<h3 class='campingtext'>" + camping.name + "</h3>" +
-        "<p class='campingtext'>" + camping.city + "</p>" +
-        "<p class='campingtext'>" + camping.price_range + "</p>" +
-        "<p class='campingtext'>" + camping.abstract + "</p>" + 
-        "<p class='campingtext'><a href='" + camping.website + "' target='_blank'>" + camping.website + "</a></p>"+
-        "<button class='campingtext'>Läs mer</button>";
-
-      container.addEventListener("click", function() {
-        window.location.href = "specifikcamping.html?id=" + camping.id;
-      });
-
-      campingDiv.appendChild(container);
+  if (campings.length === 0) { //om inga campingar med valda filter finns..
+    campingDiv.innerHTML = "<p>Inga campingar med dina valda filter finns. Justera dina filter för att hitta campingar som passar dig. </p>";
+    return; // Avbryt funktionen så att inga campingar visas
   }
 
-  
+  for (let i = 0; i < campings.length; i++) { //går igenom alla campingar :)
+    const camping = campings[i]; //hämtad camping objekt
 
+    let container = document.createElement("div");
+    container.classList.add("campcontainer"); //skapar element för de olika cmapingarna
+
+    // Skapa HTML för varje camping
+    container.innerHTML +=
+      "<h3 class='campingtext'>" + camping.name + "</h3>" +
+      "<p class='campingtext'>" + camping.city + "</p>" +
+      "<p class='campingtext'>" + camping.price_range + "</p>" +
+      "<p class='campingtext'>" + camping.abstract + "</p>" +
+      "<p class='campingtext'><a href='" + camping.website + "' target='_blank'>" + camping.website + "</a></p>"
+    //skriver ut namn, stad, pris, beskrivning och länk till webbplatsen
+
+    let button = document.createElement("button");
+    button.classList.add("campingtext");
+    button.textContent = "Läs mer"; //lägger till en "läs mer" knapp för de användare som inte fattar att man kan klicka på elementet...
+
+    container.appendChild(button) //lägger till button elementet i containern
+
+    container.addEventListener("click", function () {
+      window.location.href = "specifikcamping.html?id=" + camping.id;
+    }); //visar sidan för den camping som cklickats 
+
+    button.addEventListener("click", function () {
+      window.location.href = "specifikcamping.html?id=" + camping.id;
+    }); //visar sidan för den camping som cklickats (knappen)
+
+    campingDiv.appendChild(container); //lägger till container i campingDiv
+  }
 
 }
 //Slut displayCampings
 //-----------------------------------------------------------------------------------
 
 
-//filtrerar cmapingarn
+
+//filtrerar cmapingarn utifrån pris, stad och läge
 function filterCampings() {
-  const selectedCity = document.querySelector("#selectedCity").value; //hämtar värdet från stad filtrer
+
   const selectedPrice = document.querySelector("#selectedPrice").value; //hämtar värdet från pris filter
 
-  let filtered = allCampings;
+  let filtered = allCampings; // Startar med alla campingar – filtreras stegvis utan att påverka originaldatan
 
-  if (selectedCity !== "") { //filtrera på stad om stad är vald
-    filtered = filtered.filter(camping => camping.city === selectedCity);
+  if (selectedPrice !== "") { // Filtrera på prisklass om något valts
+    filtered = filtered.filter(function (camping) {
+      return camping.price_range === selectedPrice; // Behåll campingar med matchande pris
+    });
   }
 
-  if (selectedPrice !=="") { //filtrera på prisklass om pris är valt.
-    filtered = filtered.filter(camping => camping.price_range === selectedPrice);
+  // Hämtar alla stad-checkboxar som är ibockade
+  const selectedCheckboxesCity = document.querySelectorAll("#dropdownContentCity input[type='checkbox']:checked");
+  let selectedCities = Array.from(selectedCheckboxesCity).map(function (cb) {
+    return cb.value;
+  }); // Skapar en lista med valda stadnamn
+
+  //filtrerar campingar minst en stad är vald
+  if (selectedCities.length > 0) {
+    filtered = filtered.filter(function (camping) {
+      //behåll campingar där cmapingens stad finns med i listan av valda städer
+      return selectedCities.includes(camping.city);
+    });
   }
 
+  const selectedCheckboxes = document.querySelectorAll("#dropdownContent input[type='checkbox']:checked"); // Hämtar alla läge-checkboxar som är ibockade (skog, sjö, hav, stad)
+  let selectedPositions = Array.from(selectedCheckboxes).map(function (cb) {
+    return cb.value
+  }); // Skapar lista med valda lägen
 
-  displayCampings(filtered);
+  //filrerar vampingar om minst ett läge är valt
+  if (selectedPositions.length > 0) {
+    filtered = filtered.filter(function (camping) {
+      //behåll cmapingar där camoingens id  finns i minst en av de valda läge listorna
+      return selectedPositions.some(function (position) {
+        return positionIds[position].includes(parseInt(camping.id));
+      });
+    });
+  }
+
+  displayCampings(filtered);// Visa de campingar som matchar filtren
+
 }
 //slut filterCAmpingsByCity
 //-----------------------------------------------------------------------------------
 
 
 
-
-
 //funktion för att hämta städer från sampi
-async function fetchSmapiCities(){ //(async = asyncron kod (kod som väntar på ett svar))
+async function fetchSmapiCities() {
+  const cityContainer = document.querySelector("#dropdownContentCity"); //hämtar elementet där checkboxarna för städer ska ligga
 
-const citySelect = document.querySelector("#selectedCity") //elementet där städerna visas
-let response = await fetch ("https://smapi.lnu.se/api/?debug=true&api_key=R7PGDNjZ&controller=establishment&method=getall&descriptions=camping"); //anrop med fetch till Smapi, hämtar data
+  let response = await fetch("https://smapi.lnu.se/api/?debug=true&api_key=R7PGDNjZ&controller=establishment&method=getall&descriptions=camping"); //api antop till smpai, hämtar cmapingarna
+  const data = await response.json(); //tolkar svaret, sparar det i data
 
-const data = await response.json(); //omvandlar svaret till json objekt
+  const uniqueCities = new Set(); //används för att det inte ska bli flera av samma stad, lista där varje värde är unikt
 
-//data.payload = arrayen som innehåller campingplatser
-
-const uniqueCities = new Set(); //används för att undivka dubeltter
-
-for (let i = 0; i < data.payload.length; i++) {
+  for (let i = 0; i < data.payload.length; i++) {
     const city = data.payload[i].city;
     if (city) {
       uniqueCities.add(city);
     }
-  } //går igenom alla cmappngar och hämtar staden
+  } //loopar igenom alla campingar och hämtar namn och lägger in dom i uniqueCities listan
 
-  let sortedCities = Array.from(uniqueCities).sort(function(a, b) {
-    return a.localeCompare(b);
-  }); //sorterar städerna i alfabetisk ordning
+  let sortedCities = Array.from(uniqueCities).sort(function (a, b) {
+    return a.localeCompare(b, 'sv');
+  }); //gör om till en array och sorterar i alfabetisk ordning
 
+  for (let city of sortedCities) {
+    cityContainer.innerHTML += "<label><input type='checkbox' value='" + city + "'>" + city + "</label><br>";
+  } //skapar checkbox för varje stad och lägger in i html
 
-for (let city of sortedCities) {
-    citySelect.innerHTML += "<option>" + city + "</option>";
-} //går igenom varje stad och skapar en option i rullistan för varje stad
+  let cityCheckboxes = document.querySelectorAll("#dropdownContentCity input[type='checkbox']"); //hämtar alla checkboxar som skapats
 
-
-
+  for (let i = 0; i < cityCheckboxes.length; i++) {
+    let checkboxCity = cityCheckboxes[i];
+    checkboxCity.addEventListener("change", filterCampings);
+  } //loopar igenom checkboxarna och lägger till change eventlyssnare, anropar filter campings
 
 } //Slut smapiCities
 //---------------------------------------------
--- specifikcamping.js
+++ specifikcamping.js
@@ -1,66 +1,211 @@
 const APIkey = "R7PGDNjZ"; // API-nyckel till SMAPI
 
-let campingId;
+let campingId;  //campingens id
+let myMap; //objekt för kartan
+//let addrMarkers = [];
+let myMarkers = []; //markör för cmapingen
+let restaurantMarkers = []; //sparar markörer för resturanger i närheter
+let lat; //sparar lat
+let lng; //sparar lng
+
+
 
 function init() {
-    showSpecificCamping();  // Anropa funktionen för att visa specifik camping
+  showSpecificCamping();  // Anropa funktionen för att visa specifik camping
 
-    
 }
-
 window.addEventListener("load", init);
+//Slut init
+//_______________________________________________________________________________________________
+
+
 
 // Funktion för att visa specifik camping
 async function showSpecificCamping() {
-    const specificCampingDiv = document.querySelector(".specificCampingDiv"); // Elementet där campinginformationen ska visas
-    const urlParams = new URLSearchParams(window.location.search); // Hämtar query params från URL:en
-    campingId = urlParams.get('id'); // Hämta id från URL
 
-    console.log("campind id: ", campingId)
-    
-    
+  const specificCampingDiv = document.querySelector(".specificCampingDiv"); // Elementet där campinginformationen ska visas
 
-    // Anropa API för att hämta camping med ID
-    let response = await fetch("https://smapi.lnu.se/api/?debug=true&api_key=" + APIkey + "&controller=establishment&method=getall&descriptions=camping&ids=" + campingId);
+  const urlParams = new URLSearchParams(window.location.search); // Hämtar query params från URL:en
 
-    
+  campingId = urlParams.get('id'); // Hämta id från URL
 
-    
-  
-    const data = await response.json(); // Omvandla svaret till JSON
+  // Anropa API för att hämta camping med ID
+  let response = await fetch("https://smapi.lnu.se/api/?debug=true&api_key=" + APIkey + "&controller=establishment&method=getall&descriptions=camping&ids=" + campingId);
 
-    console.log(data.payload)
-    
-    if (data.payload.length > 0) {
-        const camping = data.payload[0]; // Hämta första (och enda) campingdata från svaret
+  const data = await response.json(); // Omvandla svaret till JSON
 
-        // Skapa och visa campinginformation på sidan
-        specificCampingDiv.innerHTML = 
-            "<h3 class='campingtext'>" + camping.name + "</h3>"+
-            "<p class='campingtext'>" + camping.city + "</p>" +
-            "<p class='campingtext'>" + camping.price_range + "</p>" +
-        
-            "<p class='campingtext'>" + camping.text + "</p>" +
-            "<p class='campingtext'><a href='" + camping.website + "' target='_blank'>" + camping.website + "</a></p>";
+  if (data.payload.length > 0) {
+    const camping = data.payload[0]; // Hämta första (och enda) campingdata från svaret
 
-            fetchReviews(campingId) // anrop av funktion för att hämta campingens recensioner
-    } 
+    // Skapa och visa campinginformation på sidan
+    specificCampingDiv.innerHTML =
+      "<h3 class='campingtext'>" + camping.name + "</h3>" +
+      "<p class='campingtext'>" + camping.city + "</p>" +
+      "<p class='campingtext'>" + camping.price_range + "</p>" +
+      "<p class='campingtext'>" + camping.text + "</p>" +
+      "<p class='campingtext'><a href='" + camping.website + "' target='_blank'>" + camping.website + "</a></p>";
 
-   
+    //fetchReviews(campingId) // anrop av funktion för att hämta campingens recensioner
+
+    console.log("campinglat: ", camping.lat, ", cmapinglng: ", camping.lng);
+
+    lat = camping.lat; //latitud
+    lng = camping.lng; //longitud
+
+    initMap(camping.lat, camping.lng, camping.name); //anropar kartan och skickar med lat, lng och namn
+
+    document.querySelector("#showRestaurantsBtn").addEventListener("click", function () {
+      fetchRestaurants(camping.lat, camping.lng);
+    }); //anropar funktion för att hämta resturanger när resturang knappen klickas på
+
+    fetchWeather(camping.lat, camping.lng); //anropar funktion för att visa väderprognosen, skickar med lat och lng
+  }
+
 }
 //slut showSpecificCamping
+//_______________________________________________________________________________________________
+
+
+//kart funktion
+function initMap(lat, lng, name) {
+
+  const zoom = 12 //zoom för kartan
+
+  myMap = L.map("map").setView([lat, lng], zoom); //skapar kartan i "map" elementet
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+  }).addTo(myMap); //ladar in kartbilden och lägger in den i kartan
+
+  // Skapa markörer
+  let marker = L.marker([lat, lng]).bindPopup(name);//skapar en markör för campingen, titeln visas vid hover visas vid
+
+  myMarkers.push(marker);//sparar markören i en array
+  marker.addTo(myMap); //lägger in markören på kartan
+
+}
+//Slut initMap
+//_______________________________________________________________________________________________
+
+
+
+//funktion för att hämta resturanger i närheter
+async function fetchRestaurants(lat, lng) {
+
+  console.log("resturang funktion")
+
+  const url = "https://smapi.lnu.se/api/?debug=true&api_key=" + APIkey + "&controller=food&method=getfromlatlng&lat=" + lat + "&lng=" + lng + "&sort_by=asc&order_by=distance_in_km"; //länken till smapi, hämtar näsmaste resturanger utifrån lat och lang
+
+  try {
+    const response = await fetch(url); //anropar apiet och väntar på svar
+    const data = await response.json(); //omvandlar svaret till ett json objekt
+
+    for (let marker of restaurantMarkers) {
+      myMap.removeLayer(marker); // Rensa gamla restaurangmarkörer från kartan
+    }
+
+    restaurantMarkers = []; //sparar marköererna i en lista
+
+    console.log("Restaurang-data:", data);
+
+    if (data.payload.length === 0) {
+      alert("Inga restauranger hittades nära denna camping.");
+      return; //om inga resturanger finns visas felmeddelande och funktion avbryts
+    }
+
+    // loopar igem alla resturanger
+    for (let i = 0; i < data.payload.length; i++) {
+      const rest = data.payload[i];
+
+      //lägger till markör och popuptext
+      const marker = L.marker([parseFloat(rest.lat), parseFloat(rest.lng)], {
+        title: rest.name
+      }).bindPopup(rest.name + "<br>" + rest.distance_in_km.toFixed(1) + " km bort");
+
+      marker.addTo(myMap); //lägger till markör på kartan
+      restaurantMarkers.push(marker) //sparar markör i listan
+    }
+
+  } catch (error) {
+    console.error("Kunde inte hämta restauranger:", error);
+  }
+}
+//Slut fetchRestaurants
+//_______________________________________________________________________________________________
+
+
+
+//funktion för att hämta väder
+async function fetchWeather(lat, lng) {
+  const url = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lng + "&daily=temperature_2m_max,temperature_2m_min&timezone=auto"; //api url för att hämta väder (Open-metero)
+
+  try {
+    const response = await fetch(url); //skickar förfrågan
+    const data = await response.json(); //gör om svaret till json
+
+    const dates = data.daily.time; //datum
+    const tempsMax = data.daily.temperature_2m_max; //maxtemperatur
+    const tempsMin = data.daily.temperature_2m_min; //min temperatur
+
+    let html = "<h4>Veckans väder prognos:</h4><div>";
+
+    //loopar igenom vare dag i prognosen
+    for (let i = 0; i < dates.length; i++) {
+      const date = new Date(dates[i]); //gör om sträng till date objekt
+      const day = date.toLocaleDateString("sv-SE", { day: 'numeric', month: 'numeric' }); // datum
+      const weekday = date.toLocaleDateString("sv-SE", { weekday: "long" });              // veckdag
+      const capitalizedWeekday = weekday.charAt(0).toUpperCase() + weekday.slice(1);      // Gör om till stor bokstav
+
+      html += "<p>" + day + " " + capitalizedWeekday + ": " + tempsMin[i] + "°C – " + tempsMax[i] + "°C</p>";
+    }
+    html += "</div>";
+    document.querySelector(".weatherDiv").innerHTML = html; //visar vädret i rätt element
+
+  } catch (error) {
+    console.error("Kunde inte hämta väderdata:", error);
+    document.querySelector(".weatherDiv").innerHTML = "Kunde inte hämta väderdata."; //loggar fel och visar felmedelande
+  }
+}
+//Slut fetchWeatcher
+//_______________________________________________________________________________________________
 
 
 
 //funktion för att visa cmapingens recensioner
-async function fetchReviews (cmapingId) {
-console.log( "showreviews funktion")
+/*async function fetchReviews(campingId) {
+    console.log("Hämtar recensioner för campingId:", campingId);
+  
+    let reviewsDiv = document.querySelector(".reviewsDiv"); //hämtar elemenet där recensionerna ska visas
+  
+    // Hämta recensioner från SMAPI
+    let response = await fetch("https://smapi.lnu.se/api/?debug=true&api_key=" + APIkey + "&controller=establishment&method=getReviews&id=" + campingId); //anropar smapi o hämtar recensioner med urlen
 
-let reviewsDiv = document.querySelector(".reviewsDiv");
+   
 
- // Anropa API för att hämta recensioner
- let response = await fetch("https://smapi.lnu.se/api/?debug=true&api_key=" + APIkey + "&controller=establishment&method=getReviews&id=" + campingId);
- const reviewsData = await response.json(); // Omvandla svaret till JSON
+  
+    const reviewsData = await response.json(); // Konvertera till JSON objekt
+  
+    console.log("Recensioner:", reviewsData.payload);
+  
+    if (reviewsData.payload.length > 0) { //kollar om det finns recensioner
+      // Visa alla recensioner
+    reviewsDiv.innerHTML = "<h3>Recensioner:</h3>";
+  
+      for (let i = 0; i < reviewsData.payload.length; i++) {
+        const review = reviewsData.payload[i];
+  
 
-console.log("cmapingidd", campingId)
-}
+        console.log("recension", reviewsData.payload[i])
+
+        reviewsDiv.innerHTML += "<div class='review'>" +
+          "<p>" + review.name + "</p>" +
+          "<p>" + review.rating + "</p>" +
+          "<p>" + review.comment + "</p>" +
+          "</div><hr>";
+      }
+  
+      
+    } else {
+      reviewsDiv.innerHTML = "<p>Inga recensioner hittades.</p>";
+    }
+  } */
